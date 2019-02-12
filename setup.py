f83a8ac8aca8ef6393d21cb811aa8fe51b773894
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-02-12 15:47:42 -0800

    Created pipenv inside lambdata

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="lambdata-danielmartinalarcon",
-    version="0.0.7",
+    version="0.0.8",
     author='Daniel Martin-Alarcon',
     description='a collection of Data Science helper functions',
     long_description=LONG_DESCRIPTION,
